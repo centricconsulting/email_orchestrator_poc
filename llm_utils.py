1385c599d7a0b7dacad0fe4fdc42a77d34c4f0ea
Joseph Ours <joseph.ours@gmail.com>
2023-09-11 10:15:49 -0400

    Modified prompt to address when it cannot determine email intent, added error trapping.

--- llm_utils.py
+++ llm_utils.py
@@ -20,7 +20,7 @@ def analyze_email(message, target_receiver):
           "Due Date": ""
         }}
 
-        If the email requires or requests an action by the receiver, provide the response in the following JSON format:
+        If the email requires or requests an action by the receiver respond in JSON format:
         {{
           "action_type": "action",
           "item_name": "[SUMMARY_OF_ACTION_REQUIRED]",
@@ -28,11 +28,19 @@ def analyze_email(message, target_receiver):
           "Due Date": "[DUE_DATE_IF_PRESENT]"
         }}
 
-        If the email is an out of office, or OOTO, notice provide the response in the following JSON format:
+        If the email is an out of office, or OOTO, notice respond in JSON format:
         {{
           "action_type": "inform",
           "item_name": "",
           "Notes": "[PERSON_WHO_IS_OUT_AND_DATE_OF_RETURN]",
+          "Due Date": ""
+        }}
+        
+        If the email does not have enough information to determine intent respond in JSON format:
+        {{
+          "action_type": "action",
+          "item_name": "Review Email - Intent Unknown",
+          "Notes": "Review Email - Intent Unknown",
           "Due Date": ""
         }}
 
@@ -72,7 +80,13 @@ def analyze_email(message, target_receiver):
 
 
 def extract_data_from_response(response):
-    return json.loads(response["choices"][0]["message"]["content"].strip())
+    res = ""
+    try:
+        res = json.loads(response["choices"][0]["message"]["content"].strip())
+    except json.JSONDecodeError:
+        append_text_to_file("errors.txt", f"ERROR PROCESSING LLM RESPONSE:\n{response}")
+        print(f"ERROR PROCESSING LLM RESPONSE:\n{response}")
+    return res
 
 
 def log_token_usage(response):
